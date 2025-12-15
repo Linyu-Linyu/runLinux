afabedeb7f025981b6caff7f2cb19f49e2d9a503
linyu <linyu22373@gmail.com>
2025-12-15 18:04:21 +0800

    modify the script to config

--- start_zsh.sh
+++ start_zsh.sh
@@ -1,4 +1,4 @@
-sh -c "$(curl -fsSL https://install.ohmyz.sh/)" -y
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 
 # install theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -6,5 +6,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # install plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+cp ./.* ~/
+cd ..
+zsh
 
